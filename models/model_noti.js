ede7eed4c451cf1a4c78dc10d64ecf00e676172c
nguyenducde <ducde678@gmail.com>
2020-05-09 19:23:51 +0700

    fix diem danh truc tiep

--- models/model_noti.js
+++ models/model_noti.js
@@ -4,7 +4,7 @@ var mongoose = require('mongoose');
 var notiSchema=new mongoose.Schema({
 //IDSuKien:,
 TenSuKien:String,
-ThoiGian:datetime.datetime.now(),
+//ThoiGian:datetime.datetime.now(),
 });
 
 var Noti=mongoose.model('Notis',notiSchema,'ThongBao');
