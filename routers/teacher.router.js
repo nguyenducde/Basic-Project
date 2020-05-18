0637a397f3277397ae90b42bd4c8c6a44cdca103
nguyenducde <ducde678@gmail.com>
2020-05-18 18:55:19 +0700

    fixview

--- routers/teacher.router.js
+++ routers/teacher.router.js
@@ -1,6 +1,6 @@
 var router=require('express').Router();
-
 var teacherController=require('../controllers/teacher.controller');
+
 router.get('/teacher',teacherController.isNotLogin_next,teacherController.getLogin);
 router.post('/teacher',teacherController.isNotLogin_next,teacherController.postLogin);
 router.get('/teacher_face',(req,res)=>{res.render('./teacher_views/teacher_face');})
