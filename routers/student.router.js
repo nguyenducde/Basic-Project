a78cd91411678e8928eb6ba25bb30fcec2155359
nguyenducde <ducde678@gmai.com>
2020-05-28 11:38:46 +0700

    anheu

--- routers/student.router.js
+++ routers/student.router.js
@@ -10,7 +10,7 @@ const multer = require('multer');
 router.get('/student',studentController.isNotLogined_next,studentController.getLogin);
 router.post('/student',studentController.isNotLogined_next,studentController.postLogin);
 router.post('/createDiemDanh',serviceActivity.upload.single('image'),studentController.saveDiemDanh);
-router.post('/upload',serviceActivity.upload.single('image'),studentController.uploadAndSave);
+router.post('/upload?c=',serviceActivity.upload.single('image'),studentController.uploadAndSave);
 router.get('/student/logout',studentController.logOut);
 router.get('/student-diemdanh',studentController.isLogined_next,studentController.getActStudent);
 
